08f1557da311773ccc7578a3f9d6e3a11fb02e44
Scott Feichter <scottfeichter@gmail.com>
2024-04-23 13:44:54 -0700

    need to test

---
+++ server/routes/colors.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const colorsRouter = express.Router();
+
+colorsRouter.get("/colors", (req, res) => {
+  res.json(`GET / colors`);
+});
+
+colorsRouter.get("/colors/:name", (req, res) => {
+  res.json(`GET /colors/:name`);
+});
